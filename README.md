5759f77ada0aa74993ce2d514f323acf792a5250
processUser <gitacc33@gmail.com>
2019-12-10 23:40:34 +0900

    2. Component Generation

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Movie App 2019
 
 React JS Fundamentals Course (2019 Update!)
+
+1. props
+2. Component Generation 
--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
 
-function Food({fav}){  //props or { fav }
-  return <h2>I like {fav}</h2>  //props.fav or { fav }
+function Food({name}){
+  return <h2>I like {name}</h2>
 }
+
+const foodILike = [
+  {name:"kimchi"},
+  {name:"ramen"},
+  {name:"samgiopsal"},
+  {name:"chukumi"},
+  {name:"kimbap"}
+];
 
 function App() {
   return (
     <div>
       <h1>Hello</h1>
-      <Food fav="kimchi" />
-      <Food fav="ramen" />
-      <Food fav="samgiopsal" />
-      <Food fav="chukumi" />
+      {foodILike.map(dish => (
+        <Food name={dish.name} />
+      ))}
     </div>
   );
 }
