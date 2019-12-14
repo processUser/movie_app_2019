508e05af070e02eed0fdacf74d9fdc2b6039682f
processUser <gitacc33@gmail.com>
2019-12-14 17:40:39 +0900

    Component Cycle

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+
 
 class App extends React.Component{
+  constructor(props) {
+    super(props);
+    console.log("hello")
+  }
   state = {
     count: 0
   };
@@ -11,7 +15,14 @@ class App extends React.Component{
   minus = () =>{
     this.setState(current => ({count: current.count - 1}));
   }
+  componentDidMount(){
+    console.log("component rendered");
+  }
+  componentDidUpdate() {
+    console.log("I just updated");
+  }
   render() {
+    console.log("I am rendering");
     return (
       <div>
         <h1>The number is: {this.state.count}</h1>
